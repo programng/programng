778cb8ee1e63dde4c7f7af645c4d7ee5eafcc19e
Jonathan Ng <jon7ng@gmail.com>
2017-03-28 09:58:04 -0700

    Change formatting, add background option

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import SocialLinks from './SocialLinks';
 
 function Landing() {
   return (
     <div className="landing">
-      <Link to="/about">About</Link>
+      {/* <Link to="/about">About</Link> */}
+      <h1 className="landing-name">JONATHAN NG</h1>
+      {/* <SocialLinks /> */}
     </div>
   );
 }
---
+++ src/SocialLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SocialLinks() {
+  return (
+    <div className="social-links">
+      <ul className="social-links-list">
+        <li className="social-links-list-item">Github</li>
+        <li className="social-links-list-item">Instagram</li>
+        <li className="social-links-list-item">Facebook</li>
+        <li className="social-links-list-item">Twitter</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SocialLinks;
