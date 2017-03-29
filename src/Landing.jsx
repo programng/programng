a6457907df73d5a3346b981fb66a0e2eafae99a7
Jonathan Ng <jon7ng@gmail.com>
2017-03-28 18:40:06 -0700

    add social media links

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-// import SocialLinks from './SocialLinks';
+import SocialLinks from './SocialLinks';
 
 function Landing() {
   return (
     <div className="landing">
       {/* <Link to="/about">About</Link> */}
       <h1 className="landing-name">JONATHAN NG</h1>
-      {/* <SocialLinks /> */}
+      <SocialLinks />
     </div>
   );
 }
